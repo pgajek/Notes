3f276fd3f7556172dca8ccea7cd5f4298d2dc073
Patryk Gajek <pgajek94@gmail.com>
2024-02-19 11:55:37 +0100

    useStorage hook

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,37 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { Container } from "react-bootstrap";
 import { Navigate, Route, Routes } from "react-router-dom";
 import NewNote from "./components/NewNote";
+import { useState } from "react";
+
+export type Tag = {
+  id: string;
+  label: string;
+};
+
+export type NoteData = {
+  title: string;
+  markdown: string;
+  tags: Tag[];
+};
+
+export type Note = {
+  id: string;
+} & NoteData;
+
+export type RawNoteData = {
+  title: string;
+  markdown: string;
+  tagIOds: string[];
+};
+
+export type RawNote = {
+  id: string;
+} & RawNoteData;
 
 function App() {
+  const [notes, setNotes] = useLocalStorage<RawNote[]>("NOTES", []);
+  const [tags, setTags] = useLocalStorage<Tag[]>("TAGS", []);
+
   return (
     <Container className="my-4">
       <Routes>
